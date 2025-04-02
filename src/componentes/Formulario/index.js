550697686a47c0b225fe986dc0cd83b11d6f62ca
Joao Pedro Rossatti Dal Agnol <jp.dalagnol@gmail.com>
2025-04-02 17:20:00 -0300

    Desenvolvimento do formulario e tratamento de inputs

---
+++ src/componentes/Formulario/index.js
@@ -0,0 +1,62 @@
+import { useState } from 'react'
+import Botao from '../Botao'
+import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
+import './Formulario.css'
+
+const Formulario = () => {
+
+    const times = [
+        'Ages I',
+        'Ages II',
+        'Ages III',
+        'Ages IV'
+    ]
+
+    const [nome, setNome] = useState('')
+    const [cargo, setCargo] = useState('')
+    const [imagem, setImagem] = useState('')
+    const [time, setTime] = useState('')
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        console.log('Form foi submetido', nome, cargo, imagem, time)
+    }
+
+    return (
+        <section className="formulario">
+            <form onSubmit={aoSalvar}>
+                <h2>Preencha os dados para criar o card do colaborador.</h2>
+                <CampoTexto
+                    obrigatorio={true}
+                    label="Nome"
+                    placeholder="Digite seu nome"
+                    valor={nome}
+                    aoAlterado={valor => setNome(valor)}
+                />
+                <CampoTexto 
+                    obrigatorio={true} 
+                    label="Cargo" 
+                    placeholder="Digite seu cargo"
+                    valor={cargo}
+                    aoAlterado={valor => setCargo(valor)}
+                />
+                <CampoTexto 
+                label="Imagem" 
+                placeholder="Digite o endereço da imagen"
+                valor={imagem}
+                aoAlterado={valor => setImagem(valor)}
+                />
+                <ListaSuspensa 
+                obrigatorio={true} 
+                label="Ages" itens={times}
+                valor={time}
+                aoAlterado={valor => setTime(valor)}
+                />
+                <Botao>Criar card</Botao>
+            </form>
+        </section>
+    )
+}
+
+export default Formulario
